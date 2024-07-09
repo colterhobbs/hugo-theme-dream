070a52b6bc25cb5f29ba5d5b6d5a9238b9c249e8
jp <jp@jptechnical.com>
2024-07-08 17:09:47 -0800

    add trmm powershell script

---
+++ static/trmm.ps1
@@ -0,0 +1,81 @@
+# author: https://github.com/bradhawkins85
+$innosetup = 'tacticalagent-v2.7.0-windows-amd64.exe'
+$api = '"https://api.jptech.support"'
+$clientid = '30'
+$siteid = '33'
+$agenttype = '"workstation"'
+$power = 0
+$rdp = 0
+$ping = 0
+$auth = '"93af22091a281fe40df9fd66b9382237aadaab72a0916e28a513e165abecad5d"'
+$downloadlink = 'https://agents.tacticalrmm.com/api/v2/agents/?version=2.7.0&arch=amd64&token=f853d451-6c02-4961-91bb-6e6958994d4f&plat=windows&api=api.jptech.support'
+$apilink = $downloadlink.split('/')
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$serviceName = 'tacticalrmm'
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    write-host ('Tactical RMM Is Already Installed')
+} Else {
+    $OutPath = $env:TMP
+    $output = $innosetup
+
+    $installArgs = @('-m install --api ', "$api", '--client-id', $clientid, '--site-id', $siteid, '--agent-type', "$agenttype", '--auth', "$auth")
+
+    if ($power) {
+        $installArgs += "--power"
+    }
+
+    if ($rdp) {
+        $installArgs += "--rdp"
+    }
+
+    if ($ping) {
+        $installArgs += "--ping"
+    }
+
+    Try
+    {
+        $DefenderStatus = Get-MpComputerStatus | select  AntivirusEnabled
+        if ($DefenderStatus -match "True") {
+            Add-MpPreference -ExclusionPath 'C:\Program Files\TacticalAgent\*'
+            Add-MpPreference -ExclusionPath 'C:\Program Files\Mesh Agent\*'
+            Add-MpPreference -ExclusionPath 'C:\ProgramData\TacticalRMM\*'
+        }
+    }
+    Catch {
+        # pass
+    }
+    
+    $X = 0
+    do {
+      Write-Output "Waiting for network"
+      Start-Sleep -s 5
+      $X += 1      
+    } until(($connectresult = Test-NetConnection $apilink[2] -Port 443 | ? { $_.TcpTestSucceeded }) -or $X -eq 3)
+    
+    if ($connectresult.TcpTestSucceeded -eq $true){
+        Try
+        {  
+            Invoke-WebRequest -Uri $downloadlink -OutFile $OutPath\$output
+            Start-Process -FilePath $OutPath\$output -ArgumentList ('/VERYSILENT /SUPPRESSMSGBOXES') -Wait
+            write-host ('Extracting...')
+            Start-Sleep -s 5
+            Start-Process -FilePath "C:\Program Files\TacticalAgent\tacticalrmm.exe" -ArgumentList $installArgs -Wait
+            exit 0
+        }
+        Catch
+        {
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            Write-Error -Message "$ErrorMessage $FailedItem"
+            exit 1
+        }
+        Finally
+        {
+            Remove-Item -Path $OutPath\$output
+        }
+    } else {
+        Write-Output "Unable to connect to server"
+    }
+}
